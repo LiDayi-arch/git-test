ecccfcc1ce65971b183daa16d9e82bbfa27a32bb
Daysi <1849417768@qq.com>
2022-05-02 14:07:21 +0800

    idea second commit

--- src/main/java/org/test/hello/gitjava.java
+++ src/main/java/org/test/hello/gitjava.java
@@ -3,5 +3,6 @@ package org.test.hello;
 public class gitjava {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
     }
 }
